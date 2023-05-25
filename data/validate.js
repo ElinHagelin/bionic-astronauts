6ef069d02778f972fe15583fc1d05b8c2e80e7b4
Forslunddavid <forslund.david@gmail.com>
2023-05-25 13:10:26 +0200

    post men skapar inte nytt id

--- data/validate.js
+++ data/validate.js
@@ -1,5 +1,25 @@
+function isValidUser(u) {
+	console.log("isValidUser 1")
+	if (typeof u !== "object") {
+		return false
+	} else if (u === null) {
+		return false
+	}
 
+	console.log("isValidUser 2")
 
+	let nameIsValid = typeof u.name === "string"
+	nameIsValid = nameIsValid && u.name !== ""
+
+	let passwordIsValid = typeof u.password === "string"
+	passwordIsValid = passwordIsValid && u.password !== ""
+
+	if (!nameIsValid || !passwordIsValid) {
+		return false
+	}
+	console.log("isValidUser 3")
+	return true
+}
 
 function isValidId(x) {
 	let maybeId = Number(x)
@@ -10,13 +30,13 @@ function isValidId(x) {
 }
 
 function findMaxId(list) {
-	let maxId = 0;
+	let maxId = 0
 	for (const item of list) {
 		if (item.id && item.id > maxId) {
-			maxId = item.id;
+			maxId = item.id
 		}
 	}
-	return maxId;
+	return maxId
 }
 
-export { isValidId, findMaxId }
+export { isValidId, findMaxId, isValidUser }
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,19 @@
 import express from "express"
 import { getDb } from "../data/database.js"
-import { isValidId } from "../data/validate.js"
+import { isValidId, isValidUser, findMaxId } from "../data/validate.js"
 
 const router = express.Router()
 const db = getDb()
+
+//GET /users
 
 router.get("/", async (req, res) => {
 	console.log("Get users", db.data.user)
 	await db.read()
 	res.send(db.data.user)
 })
+
+//GET /users/:id
 
 router.get("/:id", async (req, res) => {
 	if (!isValidId(req.params.id)) {
@@ -26,6 +30,25 @@ router.get("/:id", async (req, res) => {
 	}
 
 	res.send(maybeUser)
+})
+
+//POST /users
+
+router.post("/", async (req, res) => {
+	let maybeUser = req.body
+	console.log("Felsöker POST: maybe=", maybeUser)
+
+	if (isValidUser(maybeUser)) {
+		console.log("Felsöker POST: is valid")
+		await db.read()
+		maybeUser.id = findMaxId(db.data.user)
+		db.data.user.push(maybeUser)
+		await db.write()
+		res.status({ id: maybeUser.id })
+	} else {
+		console.log("Felsöker POST: invalid")
+		res.sendStatus(400)
+	}
 })
 
 export default router
