8efce08c29a8b8ee3460f2e28b60c5af66fe9fcc
Forslunddavid <forslund.david@gmail.com>
2023-05-30 09:53:28 +0200

    added frontend files

--- bionic-austronauts/backend/server.js
+++ bionic-austronauts/backend/server.js
@@ -1,14 +1,16 @@
 import express from "express"
-import cors from 'cors'
+import cors from "cors"
 import productRouter from "./routes/products.js"
 import userRouter from "./routes/users.js"
 import searchRouter from "./routes/search.js"
 
-
-
-
 const port = 1567
 const app = express()
+
+app.use((req, res, next) => {
+	console.log(`${req.method} ${req.url}`, req.body)
+	next()
+})
 
 app.use("/api", express.json())
 app.use(cors())
@@ -16,9 +18,6 @@ app.use(cors())
 app.use("/api/products", productRouter)
 app.use("/api/users", userRouter)
 app.use("/api/search", searchRouter)
-
-
-
 
 app.listen(port, () => {
 	console.log(`Server is listening on port ${port}...`)
